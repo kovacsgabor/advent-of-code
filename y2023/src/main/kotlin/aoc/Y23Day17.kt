484b417de6223de7b53fa404e1870e342fae57fb
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2023-12-17 18:19:24 +0100

    Make Day 17 more readable

--- y2023/src/main/kotlin/aoc/Y23Day17.kt
+++ y2023/src/main/kotlin/aoc/Y23Day17.kt
@@ -2,7 +2,7 @@ package aoc
 
 object Y23Day17 : Day() {
 
-    data class State(val pos: Pos, val dir: Dir4, val straightSteps: Int)
+    data class State(val pos: Pos, val dir: Dir4?, val straightSteps: Int)
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -12,9 +12,13 @@ object Y23Day17 : Day() {
 
         fun solve(minStraightSteps: Int, maxStraightSteps: Int): Long =
             Graph<State> { (pos, dir, straightSteps) ->
+                fun turn(d: Dir4) = State(pos + d, d, 1)
+
+                // initial state
+                if (dir == null) return@Graph listOf(turn(Dir4.RIGHT), turn(Dir4.DOWN))
+
                 val nextStates = mutableListOf<State>()
-                if (straightSteps >= minStraightSteps || straightSteps == 0) { // we can turn
-                    fun turn(d: Dir4) = State(pos + d, d, 1)
+                if (straightSteps >= minStraightSteps) { // we can turn
                     nextStates += turn(dir.right())
                     nextStates += turn(dir.left())
                 }
@@ -28,18 +32,15 @@ object Y23Day17 : Day() {
                 .dijkstra(
                     State(
                         Pos(0, 0),
-                        // The starting direction is not specified, so let us choose a direction that allows us to go
-                        // both downwards and rightwards from here.
-                        Dir4.DOWN,
-                        // Set this to 0 instead of 1, so the downward steps are not limited
-                        0,
+                        null, // the start node is a special case
+                        0 // does not matter, we handle this state in a special way
                     )
                 )
                 .filter { it.node.pos == goal && it.node.straightSteps >= minStraightSteps }
                 .first()
                 .cost
 
-        part1(solve(0, 3))
+        part1(solve(1, 3))
         part2(solve(4, 10))
     }
 
